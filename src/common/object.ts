edae03bd8c47db64afbb3551f44dcbe559ce2778
duongductrong <duongductrong06@gmail.com>
2024-08-01 01:59:30 +0700

    feat(object): :construction: add chainKey type

--- src/common/object.ts
+++ src/common/object.ts
@@ -1,1 +1,8 @@
-export type B = {};
+export type ChainKey<T> = T extends object
+  ? {
+      [K in keyof T]:
+        | K
+        // @ts-ignore
+        | `${K}${ChainKey<T[K]> extends "" ? "" : "."}${ChainKey<T[K]>}`;
+    }[keyof T]
+  : "";
