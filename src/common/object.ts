7679f520876be39d35ae93dca4baaacc15e49274
duongductrong <duongductrong06@gmail.com>
2024-08-03 02:00:58 +0700

    feat(object): :recycle: handle array on chain type

--- src/common/object.ts
+++ src/common/object.ts
@@ -1,20 +1,42 @@
 /**
- * Represents a chain key for accessing nested properties of an object.
- * @template T - The type of the object.
- * @returns The chain key.
+ * Represents a chain of keys in an object type.
+ *
+ * @template T - The object type.
+ * @template K - The keys of the object type.
+ * @returns The chain of keys.
  * @example
  * ```ts
- * type Result = Chain<{ a: { b: { c: string } } }>; // "a" | "a.b" | "a.b.c"
+ * type Example = {
+ *  a: {
+ *   b: { d: string };
+ *   x: [
+ *    { y: string; k: { l: number } },
+ *    { z2: string; k2: { l: number; m: [{ nested: number }] } }
+ *   ];
+ *  y: {
+ *   a: {
+ *    dddd: string;
+ *   };
+ *  };
+ * };
+ *
+ * type Result = Chain<Example>; // "a" | "a.b" | "a.x.0.y" | "a.x.0.k.l" | "a.x.1.z2" | "a.x.1.k2.l" | "a.x.1.k2.m.0.nested" | "a.y.a.dddd"
  * ```
  */
-export type Chain<T> = T extends object
-  ? {
-      [K in keyof T]:
-        | K
-        // @ts-ignore
-        | `${K}${Chain<T[K]> extends "" ? "" : "."}${Chain<T[K]>}`;
-    }[keyof T]
-  : "";
+export type Chain<T, K extends keyof T = keyof T> = K extends string
+  ? ExcludeArrayMethodsInChain<K> extends never
+    ? never
+    : T[K] extends Record<string, any>
+    ? K | `${K}.${Chain<T[K]>}`
+    : T[K] extends (infer U)[]
+    ? K | `${K}.${number}.${Chain<U>}`
+    : K
+  : never;
+
+type CheckArrayMethodsInChain = keyof any[];
+type ExcludeArrayMethodsInChain<T> = T extends CheckArrayMethodsInChain
+  ? never
+  : T;
 
 /**
  * Retrieves the value from an object based on the provided key.
